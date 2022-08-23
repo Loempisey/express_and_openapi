f32a0da86424f0ec825bd37c471190dce85a99d1
pisey <loempiseydec25@gmail.com>
2022-08-23 09:34:36 +0700

    make change on doc

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,5 @@
 const controller = require('./../controllers/user.controller');
 module.exports = (app)=>{
-    app.post('/user', controller.createUser)
+    app.post('/user/signup', controller.createUser)
     app.post('/user/signin', controller.createSignIn)
 }
